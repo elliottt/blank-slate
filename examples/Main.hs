2dc22bb6e4f6207b8f3da1d52071158f5027e540
Trevor Elliott <trevor@galois.com>
2012-10-02 18:12:39 -0700

    Initialize the opengl drawing context

--- examples/Main.hs
+++ examples/Main.hs
@@ -5,6 +5,5 @@ import Graphics.BlankSlate
 import Control.Concurrent (threadDelay)
 
 
-main = withGraphics $ do
-  win <- initGraphics "Test" 640 480
-  threadDelay 1000000
+main :: IO ()
+main  = withGraphics "Test" 640 480 (threadDelay 1000000)
--- src/Graphics/BlankSlate.hs
+++ src/Graphics/BlankSlate.hs
@@ -6,10 +6,35 @@ module Graphics.BlankSlate (
 
 import Graphics.BlankSlate.Resource as Exports
 import Graphics.BlankSlate.Texture  as Exports
-import Graphics.BlankSlate.Window   as Exports
 
 import Graphics.UI.SDL as SDL
+import Graphics.Rendering.GLU.Raw
+import Graphics.Rendering.OpenGL.Raw.Core21
+import Graphics.Rendering.OpenGL.Raw.ARB.Compatibility
 
 
-withGraphics :: IO a -> IO a
-withGraphics  = SDL.withInit [InitEverything]
+withGraphics :: String -> Int -> Int -> IO a -> IO a
+withGraphics title width height body = withInit [InitEverything] $ do
+  -- SDL
+  SDL.setCaption title ""
+  _ <- SDL.setVideoMode width height 24 [OpenGL]
+
+  -- OpenGL
+  glClearColor 0 0 0 1.0
+  glClearDepth 1
+  glDepthFunc  gl_LESS
+  glShadeModel gl_SMOOTH
+
+  glMatrixMode gl_PROJECTION
+  glLoadIdentity
+  gluPerspective 45 (fromIntegral width / fromIntegral height) 0.1 100
+
+  glMatrixMode gl_MODELVIEW
+
+  glEnable gl_TEXTURE_2D
+  glEnable gl_BLEND
+  glBlendFunc gl_SRC_ALPHA gl_ONE_MINUS_SRC_ALPHA
+
+  glFlush
+
+  body
