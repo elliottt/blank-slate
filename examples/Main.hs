56c0266053f1c3f778f3eab839255bdf02bdad53
Trevor Elliott <awesomelyawesome@gmail.com>
2012-10-18 21:26:58 -0700

    Polling events is required in the example
      If you don't poll for events on windows, eventually the program will be
    assumed to not be responding, and killed.

--- examples/Main.hs
+++ examples/Main.hs
@@ -8,9 +8,12 @@ import Control.Monad (forever)
 import Data.Array (Array,listArray)
 import Data.Array.Storable (thaw)
 import Foreign (nullPtr)
+import System.Exit (exitSuccess)
 
 import Graphics.Rendering.OpenGL.Raw.ARB.Compatibility
 import Graphics.Rendering.OpenGL.Raw.Core21
+
+import Graphics.UI.SDL as SDL
 
 
 verts :: Array Int Float
@@ -21,46 +24,51 @@ verts = listArray (0,11)
   ]
 
 main :: IO ()
-main  = withGraphics "Test" 640 480           $ \ win  ->
-      bracket newProgram        deleteProgram $ \ pgm  ->
-      bracket newVertexShader   deleteShader  $ \ vert ->
-      bracket newFragmentShader deleteShader  $ \ frag -> do
+main  =
+  withGraphics "Test" 640 480           $ \ win  ->
+  bracket newProgram        deleteProgram $ \ pgm  ->
+  bracket newVertexShader   deleteShader  $ \ vert ->
+  bracket newFragmentShader deleteShader  $ \ frag -> do
 
-        loadShaderFromFile vert "examples/vert.glsl"
-        loadShaderFromFile frag "examples/frag.glsl"
+    loadShaderFromFile vert "examples/vert.glsl"
+    loadShaderFromFile frag "examples/frag.glsl"
 
-        attachShader pgm vert
-        attachShader pgm frag
-        linkProgram pgm
-        useProgram pgm
+    attachShader pgm vert
+    attachShader pgm frag
+    linkProgram pgm
+    useProgram pgm
 
-        -- ship the triangle data to the card
-        triangle     <- genBuffer
-        triangleData <- thaw verts
-        bindBuffer gl_ARRAY_BUFFER triangle
-        bufferData gl_ARRAY_BUFFER triangleData gl_STATIC_DRAW
-        glVertexPointer 4 gl_FLOAT 0 nullPtr
-        unbindBuffer gl_ARRAY_BUFFER
+    -- ship the triangle data to the card
+    triangle     <- genBuffer
+    triangleData <- thaw verts
+    bindBuffer gl_ARRAY_BUFFER triangle
+    bufferData gl_ARRAY_BUFFER triangleData gl_STATIC_DRAW
+    glVertexPointer 4 gl_FLOAT 0 nullPtr
+    unbindBuffer gl_ARRAY_BUFFER
 
-        glEnableClientState gl_VERTEX_ARRAY
+    glEnableClientState gl_VERTEX_ARRAY
 
-        glMatrixMode gl_MODELVIEW
-        glLoadIdentity
+    glMatrixMode gl_MODELVIEW
+    glLoadIdentity
 
-        glMatrixMode gl_PROJECTION
-        glLoadIdentity
+    glMatrixMode gl_PROJECTION
+    glLoadIdentity
 
-        forever $ do
+    forever $ do
+      evt <- SDL.pollEvent
+      case evt of
+        SDL.Quit -> exitSuccess
+        _        -> return ()
 
-          glClear (fromIntegral gl_COLOR_BUFFER_BIT)
+      glClear (fromIntegral gl_COLOR_BUFFER_BIT)
 
-          glMatrixMode gl_MODELVIEW
-          glLoadIdentity
+      glMatrixMode gl_MODELVIEW
+      glLoadIdentity
 
-          bindBuffer gl_ARRAY_BUFFER triangle
-          glDrawArrays gl_TRIANGLES 0 3
-          unbindBuffer gl_ARRAY_BUFFER
+      bindBuffer gl_ARRAY_BUFFER triangle
+      glDrawArrays gl_TRIANGLES 0 3
+      unbindBuffer gl_ARRAY_BUFFER
 
-          flush win
+      flush win
 
-          threadDelay 100000
+      threadDelay 1000
