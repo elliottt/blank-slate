48701025e880c3a843db0e60db9755849711787a
Trevor Elliott <trevor@galois.com>
2013-06-13 18:10:16 -0700

    JuicyPixels image to textures

--- src/Graphics/BlankSlate/Prim/Texture.hs
+++ src/Graphics/BlankSlate/Prim/Texture.hs
@@ -1,13 +1,24 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Graphics.BlankSlate.Prim.Texture (
     Texture(..)
   , newTexture, newTextures
   , freeTexture, freeTextures
+  , textureImage2d
+  , PixelFormat()
   ) where
 
+import Codec.Picture.Types
+    (Image(..),Pixel(..),Pixel8,PixelF,PixelYA8,PixelRGB8,PixelRGBA8,PixelRGBF)
+import Data.Vector.Storable (unsafeWith)
 import Foreign (allocaArray,peekArray,withArrayLen)
+import Graphics.Rendering.OpenGL.Raw.ARB.Compatibility
 import Graphics.Rendering.OpenGL.Raw.Core31
+import qualified Control.Exception as X
 
 
 newtype Texture = Texture
@@ -36,3 +47,54 @@ freeTexture tex = freeTextures [tex]
 freeTextures :: [Texture] -> IO ()
 freeTextures texs =
     withArrayLen (map getTexture texs) (glDeleteTextures . fromIntegral)
+
+-- | Turn a JuicyPixels 'Image' into an OpenGL textures.
+textureImage2d :: (PixelFormat a)
+               => Texture
+               -> Image a
+               -> Int      -- ^ MIP map level
+               -> Int      -- ^ Border (0 or 1)
+               -> IO ()
+textureImage2d tex image level border = do
+  glBindTexture gl_TEXTURE_2D (getTexture tex)
+  unsafeWith (imageData image) body `X.finally` glBindTexture gl_TEXTURE_2D 0
+  where
+  body = glTexImage2D
+      gl_TEXTURE_2D
+      (fromIntegral level)
+      (pixelInternalFormat image)
+      (fromIntegral (imageWidth  image))
+      (fromIntegral (imageHeight image))
+      (fromIntegral border)
+      (pixelFormat image)
+      (pixelType image)
+
+class Pixel a => PixelFormat a where
+  pixelInternalFormat :: Image a -> GLint
+  pixelInternalFormat  = fromIntegral . pixelFormat
+  pixelFormat         :: Image a -> GLenum
+  pixelType           :: Image a -> GLenum
+
+instance PixelFormat Pixel8 where
+  pixelFormat _ = gl_LUMINANCE
+  pixelType _   = gl_UNSIGNED_BYTE
+
+instance PixelFormat PixelF where
+  pixelFormat _ = gl_LUMINANCE
+  pixelType _   = gl_FLOAT
+
+instance PixelFormat PixelYA8 where
+  pixelFormat _ = gl_LUMINANCE_ALPHA
+  pixelType _   = gl_UNSIGNED_BYTE
+
+instance PixelFormat PixelRGB8 where
+  pixelFormat _ = gl_RGB
+  pixelType _   = gl_UNSIGNED_BYTE
+
+instance PixelFormat PixelRGBF where
+  pixelFormat _ = gl_RGB
+  pixelType _   = gl_FLOAT
+
+instance PixelFormat PixelRGBA8 where
+  pixelFormat _ = gl_RGBA
+  pixelType _   = gl_UNSIGNED_BYTE
